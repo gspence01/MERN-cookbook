1315765a9f444b94e980956d7107eccf44888722
Gabrielle <gabrielle01.spence@gmail.com>
2023-03-26 22:43:50 -0700

    changed recipe seeder file to create more cards in gallery container

--- server/seeders/20230325045452-recipes.js
+++ server/seeders/20230325045452-recipes.js
@@ -15,8 +15,44 @@ module.exports = {
    await queryInterface.bulkInsert('recipes', [{
     recipe_name: 'Ackee and Saltfish',
     description: "A salty, spicy, breakfast item made with Jamaica's national fruit",
+    pic: "../../public/ackee-pic.jpg",
     is_faved: false,
-    date: '2022-01-17'
+    upload_date: '2022-01-17'
+   },
+   {
+    recipe_name: 'Festival',
+    description: "A fried cornmeal dumpling that goes well with any Jamican entree.",
+    pic: "../../public/festival.jpg",
+    is_faved: false,
+    upload_date: '2021-01-17'
+   },
+   {
+    recipe_name: 'Curry Goat',
+    description: "A spicy curry full of goat, aromatics, and vegetables.",
+    pic: "../../public/jamaican-goat-curry.jpg",
+    is_faved: false,
+    upload_date: '2022-05-20'
+   },
+   {
+    recipe_name: 'Jerk Chicken',
+    description: "A classic Jamaican dish you will find at any shop.",
+    pic: "../../public/jerk_chicken.jpg",
+    is_faved: false,
+    upload_date: '2023-01-17'
+   },
+   {
+    recipe_name: 'Mackerel Rundown',
+    description: "This is an old Jamaican classic, known for its sweetness and spiciness.",
+    pic: "../../public/mackerel.jpeg",
+    is_faved: false,
+    upload_date: '2022-01-17'
+   },
+   {
+    recipe_name: 'Oxtail',
+    description: "This is a spicy stew-like dish with fall-off-the-bone oxtail simmered in a delicious broth",
+    pic: "../../public/oxtail.jpg",
+    is_faved: false,
+    upload_date: '2022-01-17'
    }], {})
   },
 
